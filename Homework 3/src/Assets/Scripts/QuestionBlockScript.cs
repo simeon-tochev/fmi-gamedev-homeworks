003314f9a7bfb66c187408ebc8a89eb77ee2614d
simeon-tochev <simeon_tochev98@abv.bg>
2020-04-10 21:18:46 +0300

    Homework 3

---
+++ Homework 3/src/Assets/Scripts/QuestionBlockScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionBlockScript : MonoBehaviour
+{
+    public GameObject mushroom;
+    public Sprite emptyBlock;
+    public Vector3 spawnOffset;
+
+    private bool hit;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.name == "Mario" && !hit) {
+            hit = true;
+            GameObject.Instantiate(mushroom, transform.position + spawnOffset, transform.rotation);
+            GetComponent<SpriteRenderer>().sprite = emptyBlock;
+        }
+    }
+}
